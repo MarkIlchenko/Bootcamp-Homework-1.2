14337085a18e679cd9b358e0b27ce07aed7efc99
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-07-18 13:23:27 +0300

    Made changes

--- main.js
+++
@@ -1,8 +0,0 @@
-const burgerIcon = document.getElementById("Burger_menu");
-const linksContainer = document.querySelector(".links-container");
-
-console.log(burgerIcon);
-
-burgerIcon.addEventListener("click", () => {
-    linksContainer.classList.toggle("unvisible");
-})
